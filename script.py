45d9bed6a3386d8f402a0347e3aebecd2308e06c
DJ <social.deej@gmail.com>
2020-09-01 10:18:28 +0530

    Completed The Work

--- script.py
+++ script.py
@@ -9,14 +9,18 @@ for f in os.listdir():
         word_for_rename =''
         original_constraint = ''
         for index in range (4):
-            original_constraint = original_constraint +  list_for_name[index]+ '_' 
+            if (list_for_name[0]=='FALLSEM2020-21'):
+                original_constraint = original_constraint +  list_for_name[index]+ '_' 
         for index in range (4,len(list_for_name)):
             if (index == len(list_for_name)-1):
                 word_for_rename = word_for_rename +list_for_name[index]
             else :
                 word_for_rename = word_for_rename+list_for_name[index] + '_'  
         merged_original_words = original_constraint + word_for_rename
-        print(merged_original_words)
-        os.rename(merged_original_words, word_for_rename)
+        print(type(original_constraint))
+        if (original_constraint == ''):
+            print('The Script Has Already Worked , Thank You')
+        else :
+            os.rename(merged_original_words, word_for_rename)
 
 
