22853c668fae4c6cd28e8b655accc42642c26e17
DJ <social.deej@gmail.com>
2020-09-01 09:42:57 +0530

    Getting Names For The Files Allocation

--- script.py
+++ script.py
@@ -4,5 +4,6 @@ import shutil
 os.chdir('/home/nopc/Desktop/Github-Clones/Rename-Rep/Formal Language and Theory Of Automation')
 
 for f in os.listdir():
-    print(f+'\n')
+    if (len(f.split('_'))) > 4 :
+        print(f.split('_'))
 
