3f4da63b0cdcb2e586558648b31b00b4a2600a3a
DJ <social.deej@gmail.com>
2020-09-01 09:48:21 +0530

    Logging File Names to List

--- script.py
+++ script.py
@@ -5,5 +5,10 @@ os.chdir('/home/nopc/Desktop/Github-Clones/Rename-Rep/Formal Language and Theory
 
 for f in os.listdir():
     if (len(f.split('_'))) > 4 :
-        print(f.split('_'))
+        list_for_name = f.split('_')
+        word =''
+        for index in range (4,):
+            word = word + list_for_name[index] 
+        print(word)
+
 
