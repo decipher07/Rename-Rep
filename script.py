c28faa046602dbdee124372b4c398f509e1e4ebf
DJ <social.deej@gmail.com>
2020-09-01 10:09:47 +0530

    Need to Restrict for A Single Iteration

--- script.py
+++ script.py
@@ -6,9 +6,17 @@ os.chdir('/home/nopc/Desktop/Github-Clones/Rename-Rep/Formal Language and Theory
 for f in os.listdir():
     if (len(f.split('_'))) > 4 :
         list_for_name = f.split('_')
-        word =''
-        for index in range (4,):
-            word = word + list_for_name[index] 
-        print(word)
+        word_for_rename =''
+        original_constraint = ''
+        for index in range (4):
+            original_constraint = original_constraint +  list_for_name[index]+ '_' 
+        for index in range (4,len(list_for_name)):
+            if (index == len(list_for_name)-1):
+                word_for_rename = word_for_rename +list_for_name[index]
+            else :
+                word_for_rename = word_for_rename+list_for_name[index] + '_'  
+        merged_original_words = original_constraint + word_for_rename
+        print(merged_original_words)
+        os.rename(merged_original_words, word_for_rename)
 
 
